8517bc2d55dc69eabf55ca722714817d108001ca
vanchien-123 <vanchien0830gmail.com>
2023-05-26 20:04:38 +0700

    upload

---
+++ DoctorWebForum/Controllers/FeedbacksController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoctorWebForum.Data;
+using DoctorWebForum.Models;
+
+namespace DoctorWebForum.Controllers
+{
+    public class FeedbacksController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public FeedbacksController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Feedbacks
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Feedback.Include(f => f.Post).Include(f => f.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Feedbacks/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Feedback == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedback
+                .Include(f => f.Post)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Create
+        public IActionResult Create()
+        {
+            ViewData["PostId"] = new SelectList(_context.Post, "Id", "Id");
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id");
+            return View();
+        }
+
+        // POST: Feedbacks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Content,UserId,ChildId,Level,IsRoot,PostId")] Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(feedback);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostId"] = new SelectList(_context.Post, "Id", "Id", feedback.PostId);
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", feedback.UserId);
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Feedback == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedback.FindAsync(id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+            ViewData["PostId"] = new SelectList(_context.Post, "Id", "Id", feedback.PostId);
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", feedback.UserId);
+            return View(feedback);
+        }
+
+        // POST: Feedbacks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Content,UserId,ChildId,Level,IsRoot,PostId")] Feedback feedback)
+        {
+            if (id != feedback.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(feedback);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FeedbackExists(feedback.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PostId"] = new SelectList(_context.Post, "Id", "Id", feedback.PostId);
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", feedback.UserId);
+            return View(feedback);
+        }
+
+        // GET: Feedbacks/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Feedback == null)
+            {
+                return NotFound();
+            }
+
+            var feedback = await _context.Feedback
+                .Include(f => f.Post)
+                .Include(f => f.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            return View(feedback);
+        }
+
+        // POST: Feedbacks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Feedback == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Feedback'  is null.");
+            }
+            var feedback = await _context.Feedback.FindAsync(id);
+            if (feedback != null)
+            {
+                _context.Feedback.Remove(feedback);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FeedbackExists(int id)
+        {
+          return (_context.Feedback?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ DoctorWebForum/Controllers/Posts1Controller.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DoctorWebForum.Data;
+using DoctorWebForum.Models;
+
+namespace DoctorWebForum.Controllers
+{
+    public class Posts1Controller : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public Posts1Controller(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts1
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Post.Include(p => p.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Posts1/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Post == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts1/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id");
+            return View();
+        }
+
+        // POST: Posts1/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,HtmlContent,UserId")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", post.UserId);
+            return View(post);
+        }
+
+        // GET: Posts1/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Post == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", post.UserId);
+            return View(post);
+        }
+
+        // POST: Posts1/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,HtmlContent,UserId")] Post post)
+        {
+            if (id != post.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.ApplicationUser, "Id", "Id", post.UserId);
+            return View(post);
+        }
+
+        // GET: Posts1/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Post == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Post
+                .Include(p => p.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts1/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Post == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Post'  is null.");
+            }
+            var post = await _context.Post.FindAsync(id);
+            if (post != null)
+            {
+                _context.Post.Remove(post);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+          return (_context.Post?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
